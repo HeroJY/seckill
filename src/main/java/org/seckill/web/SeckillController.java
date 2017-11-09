2eb396c376c53d4d49f1b1b6489667f64bdc8c57
764479146@qq.com <940207cHJY>
2017-11-09 10:47:21 +0800

    按照阿里规约修改代码规范

--- src/main/java/org/seckill/web/SeckillController.java
+++ src/main/java/org/seckill/web/SeckillController.java
@@ -3,6 +3,7 @@ package org.seckill.web;
 import java.util.Date;
 import java.util.List;
 
+import javafx.application.Application;
 import org.seckill.dto.Exposer;
 import org.seckill.dto.SeckillExecution;
 import org.seckill.dto.SeckillResult;
@@ -14,6 +15,7 @@ import org.seckill.service.SeckillService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -21,6 +23,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author hjy
@@ -31,18 +36,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SeckillController {
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
+
 	@Autowired
 	private SeckillService seckillService;
-	
-	//秒杀商品列表页面Controller
+
+	/**
+	 * 秒杀商品列表页面Controller
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value="/list",method = RequestMethod.GET)
 	public String list(Model model){
 		//获取页面列表
 		List<Seckill> seckills = seckillService.getSeckillList();
 		model.addAttribute("list", seckills);
 		//返回list.jsp+model=ModelAndView
-		return "list";///WEB-INF/jsp/list.jsp
+		///WEB-INF/jsp/list.jsp
+		return "list";
 	}
 	
 	//秒杀商品详情页面Controller
@@ -58,10 +69,15 @@ public class SeckillController {
 		model.addAttribute("seckill",seckill);
 		return "detail";
 	}
-	
-	//根据商品Id提供md5加密后的独一无二的访问url Controller
-	//ajax json接口所以不需要Model
-	//produces告诉浏览器返回类型，同时解决页面乱码问题
+
+
+	/**
+	 * 	根据商品Id提供md5加密后的独一无二的访问url Controller
+	 * 	ajax json接口所以不需要Model
+	 * 	produces告诉浏览器返回类型，同时解决页面乱码问题
+	 * @param seckillId
+	 * @return
+	 */
 	@RequestMapping(value="/{seckillId}/exposer",method = RequestMethod.POST,produces = {"application/json;charset=UTF-8"})
 	@ResponseBody//SpringMVC扫描到该注解会自动把返回类型封装成json
 	public SeckillResult<Exposer> exporser(@PathVariable("seckillId") Long seckillId){
@@ -75,8 +91,14 @@ public class SeckillController {
 		}
 		return result;
 	}
-	
-	//通过独一无二的url 执行秒杀逻辑Controller
+
+	/**
+	 * 通过独一无二的url 执行秒杀逻辑Controller
+	 * @param seckillId
+	 * @param md5
+	 * @param userPhone
+	 * @return
+	 */
 	@RequestMapping(value="/{seckillId}/{md5}/execution",method = RequestMethod.POST,produces = {"application/json;charset=UTF-8"})
 	@ResponseBody
 	//@CookieValue中如果不设置required = false那么只要请求的RequestHeader中没有killPhone那么就会报错
@@ -101,8 +123,11 @@ public class SeckillController {
 			return new SeckillResult<SeckillExecution>(false, execution);
 		}
 	}
-	
-	//返回系统时间Controller
+
+	/**
+	 * 返回系统时间Controller
+	 * @return
+	 */
 	@RequestMapping(value = "/time/now",method=RequestMethod.GET)
 	@ResponseBody
 	public SeckillResult<Long> time(){
